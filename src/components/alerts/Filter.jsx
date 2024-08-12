776c2ceec09dd2e3c3f1c4f48293b63ca4d9f620
Suman Sakshi <sumansakshi711@gmail.com>
2024-08-12 23:34:17 +0530

    improved ui

--- src/components/alerts/Filter.jsx
+++ src/components/alerts/Filter.jsx
@@ -24,35 +24,31 @@ const SearchBar = ({ setSearchText, setVehicleNumber, setStartDate, setEndDate, 
 
   return (
     <div className="flex items-center space-x-4 mb-4 max-sm:flex-col max-sm:gap-4">
-      <input type="text" placeholder="Search" className="p-2 border rounded w-full" onChange={(e) => setSearchText(e.target.value)} />
+      <input type="text" placeholder="Search" className="p-2 border rounded w-full h-[50px]" onChange={(e) => setSearchText(e.target.value)} />
       <div className="w-full">
-        <Select options={vehicleOptions} placeholder="Vehicle #" className="text-base" onChange={(selectedOption) => setVehicleNumber(selectedOption.label)} />
+        <Select options={vehicleOptions} placeholder="Vehicle #" className="text-base" onChange={(selectedOption) => setVehicleNumber(selectedOption.label)} styles={{ control: (base) => ({ ...base, height: "50px" }) }} />
       </div>
       <div className="flex space-x-4 w-full">
         <label className="flex items-center space-x-2">
           <span className="font-bold">Date Range:</span>
           <input
             type="date"
-            className="p-2 border rounded"
+            className="p-2 border rounded h-[50px]"
             value={startDate}
             onChange={(e) => {
               const newStartDate = e.target.value;
-              if (newStartDate > endDate) {
-                setEndDate(newStartDate);
-              }
+
               setStartDate(newStartDate);
             }}
           />
           <span className="mx-1">to</span>
           <input
             type="date"
-            className="p-2 border rounded"
+            className="p-2 border rounded h-[50px]"
             value={endDate}
             onChange={(e) => {
               const newEndDate = e.target.value;
-              if (newEndDate < startDate) {
-                setStartDate(newEndDate);
-              }
+
               setEndDate(newEndDate);
             }}
           />
